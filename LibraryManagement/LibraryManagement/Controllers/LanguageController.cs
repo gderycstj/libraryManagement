aa074ae1878efda14aeea553d463b1304bd68115
zladix <vincent.leclerc21@hotmail.fr>
2016-10-15 21:12:25 -0400

    Test#2
    PLS WORK

---
+++ LibraryManagement/LibraryManagement/Controllers/LanguageController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagement.Utils;
+
+namespace LibraryManagement.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult setLanguage() 
+        {
+            UtilResources.ChangeLanguageTwo();
+            string url = this.Request.UrlReferrer.AbsolutePath;
+            return Redirect(url);
+        }
+    }
+}
